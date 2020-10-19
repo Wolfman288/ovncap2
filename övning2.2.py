84e5660abc530f9adb1eaaeeaad8b58f3b25af45
Wolfman288 <45688865+Wolfman288@users.noreply.github.com>
2020-10-19 14:42:51 +0200

    Övning 2.2

---
+++ övning2.2.py
@@ -0,0 +1,13 @@
+import math
+
+r = False
+v = False
+
+r = input('Vad är radien?: ')
+r = float(r)
+v = float(v)
+v = 4 * 3.14 * r * r * r #formeln till volymen på en sfär
+v = v / 3
+
+print(v)
+# Den här koden räknar volymen på en sfär
